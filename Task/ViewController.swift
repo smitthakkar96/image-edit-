13cf9cfb78e04309e1f3db1353dcd15e32a6f1ae
smit thakkar <sarangkachhadiya@gmail.com>
2016-04-04 14:27:56 +0530

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by smit thakkar on 04/04/16.
+//  Copyright © 2016 smit thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
